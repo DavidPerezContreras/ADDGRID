c6c03bcd79bce3b9b478f4465dd3827d370f5dba
ldubi01 <ldubi01@ubuntu>
2021-09-26 15:53:30 -0700

    CASI TERMINADO 27/9/2021

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -34,6 +38,9 @@ public class Main extends Application{
         Button btn4 = new Button();
         Button btn5 = new Button();
         
+        //Crea una etiqueta (Label)
+        Label label = new Label("Example");
+        
         
         //ESCRIBE EN LOS BOTONES
         btn.setText("Say 'Hello World'");
@@ -45,25 +52,29 @@ public class Main extends Application{
 		
         
         //SET ESPACIO ENTRE CELDAS
-
+        //gp1.setVgap(10);
+        //gp1.setHgap(10);
 		
 		
-        gp1.add(btn, 0,0);
+        gp1.add(label, 0,0);
         gp1.add(btn2, 1, 1);
         gp1.add(btn3, 2, 2);
         gp1.add(btn4, 3, 3);
         gp1.add(btn5, 4, 4);
+        
+        //RowSpan
+       GridPane.setRowSpan(label, 1);
+        
+
+        
+        //Alinea el GridPane en el centro del contenedor raiz
+        //gp1.setAlignment(Pos.CENTER);
 		
-		root.getChildren().add(gp1);
 		
+		
+        root.getChildren().add(gp1);
 		gp1.setGridLinesVisible(true);
-		
-		
-		
-		
-		
-		
-		primaryStage.setScene(new Scene(root, 1920, 1050));
+		primaryStage.setScene(new Scene(root, 700, 200));
         primaryStage.show();
 	}
 
